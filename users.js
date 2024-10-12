1e4253fca2be766636268204ac1ab5363f1247cc
rishabh32700 <rishabhpep@gmail.com>
2024-10-12 11:20:12 +0530

    Added my details

--- users.js
+++
@@ -1,9 +0,0 @@
-const users = [
-    {
-        id : 0,
-        name : "rishabh",
-        github_Username : "Rishabh32700",
-        github_Profile : "https://github.com/Rishabh32700",
-        any_open_source_contribution_so_far : "Yes",
-    }
-]

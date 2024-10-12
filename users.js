cb376cd2a6db3915c143f7c69a60b82edbe47451
rishabh32700 <rishabhpep@gmail.com>
2024-10-12 11:12:50 +0530

    updated users.js

---
+++ users.js
@@ -0,0 +1,9 @@
+const users = [
+    {
+        id : 0,
+        name : "rishabh",
+        github_Username : "Rishabh32700",
+        github_Profile : "https://github.com/Rishabh32700",
+        any_open_source_contribution_so_far : "Yes",
+    }
+]
